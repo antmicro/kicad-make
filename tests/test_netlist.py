3e37504ed55eaebb067a380c2b6bc0f8d0e92240
Pawel Koszalka <pkoszalka@internships.antmicro.com>
2024-04-29 16:46:57 +0200

    [#58706] tests: Add test for netlist
    [#58706] tests: Move setUp to top
    
    [#58706] tests: Add docstrings
    
    [#58706] tests: Add netlist to reference output
    
    [#58706] tests: Remove unused code
    
    [#58706] tests: Reformat code
    
    [#58706] tests: Use variables instead of hardcoded paths
    
    [#58706] tests: Fix netlist test
    
    [#58706] tests: Fix path in netlist
    
    [#58706] tests: Add artifacts to the CI
    
    [#58706] tests: Use netlist from CI
    
    [#58706] tests: Remove update paths
    
    [#58706] tests: Use CM4 baseboard
    
    [#58706] tests: Fix newlines in netlist
    
    [#58706] tests: Fix tests for netlist

---
+++ tests/test_netlist.py
@@ -0,0 +1,74 @@
+import unittest
+import kmake
+import re
+import os
+from pathlib import Path
+from git import Repo
+from common.kicad_project import KicadProject
+
+
+TEST_COMMAND = "netlist"
+TEST_DIR = Path(__file__).parent.resolve()
+# path to test design repository
+TARGET = TEST_DIR / "test-designs" / "sdi-mipi-bridge-hw"
+REF_OUTS = TEST_DIR / "reference-outputs" / "sdi-mipi-bridge" / "netlist"
+
+
+class BomNetlist(unittest.TestCase):
+    def setUp(self) -> None:
+        """Prepare test data"""
+        # make sure test design repository doesn't have any changes
+        test_repo = Repo(TARGET)
+        test_repo.git.reset("--hard", "HEAD")
+        test_repo.git.clean("-fd")
+        os.chdir(TARGET)
+
+    def test_netlists_equal(self) -> None:
+        """Test if target and reference netlists are equal"""
+        self.args = kmake.parse_arguments([TEST_COMMAND])
+        self.kpro = KicadProject()
+
+        self.args.func(self.kpro, self.args)
+
+        ref = list(open(REF_OUTS / "netlist.net"))
+        tar = list(open(TARGET / "fab" / "netlist.net"))
+
+        lines_to_remove = []
+        uri_match = False
+        source_match = False
+        tool_match = False
+
+        for line_id, line in enumerate(tar):
+            if re.search(r"\(uri", line) is not None:
+                lines_to_remove.append(line_id)
+                uri_match = True
+            if re.search(r'\(source\s"/', line):
+                lines_to_remove.append(line_id)
+                source_match = True
+            if (
+                re.search(
+                    r"\(date\s",
+                    line,
+                )
+                is not None
+            ):
+                lines_to_remove.append(line_id)
+            if re.search(r'\(tool\s"Eeschema\s\d\.\d\.\d', line) is not None:
+                lines_to_remove.append(line_id)
+                tool_match = True
+
+        counter = 0
+        for line_id in sorted(lines_to_remove):
+            del ref[line_id - counter]
+            del tar[line_id - counter]
+            counter += 1
+
+        self.assertTrue(uri_match)
+        self.assertTrue(source_match)
+        self.assertTrue(tool_match)
+
+        # Check if every element was matched:uri, source, tool version, date
+        # the date occurs two times (in the netlist header and title block section)
+        self.assertEqual(len(lines_to_remove), 5)
+
+        self.assertListEqual(tar, ref)
