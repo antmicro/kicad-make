6d3c8015843ae289654ca6764b551ed4c1ed7556
Dawid Lentka <dlentka@antmicro.com>
2024-06-04 08:38:12 +0200

    [#60273] rename: Stop adding empty lines

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -13,6 +13,7 @@ TEST_DIR = Path(__file__).parent.resolve()
 JETSON_ORIN_BASEBOARD_DIR = TEST_DIR / "test-designs" / "jetson-orin-baseboard"
 RESULT_DIR = TEST_DIR / "results" / TEST_NAME
 
+
 class RenameTest(unittest.TestCase):
     def setUp(self) -> None:
         kicad_project_repo = Repo(JETSON_ORIN_BASEBOARD_DIR)
@@ -26,16 +27,36 @@ class RenameTest(unittest.TestCase):
         self.args.func(self.kpro, self.args)
 
     def tearDown(self) -> None:
+        whitelist = [
+            ".kicad_pro",
+            ".kicad_pcb",
+            ".kicad_sch",
+            ".kicad_mod",
+            ".kicad_sym",
+            ".kicad_prl",
+            ".kicad_dru",
+            ".md",
+            ".txt",
+            ".rst",
+            ".json",
+            ".csv",
+            ".gbr",
+            ".svg",
+            ".xml",
+            "sym-lib-table",
+            "fp-lib-table",
+            "fp-cache-table",
+        ]
         for file in Path(self.kpro.dir).rglob("*"):
             file = file.relative_to(self.kpro.dir)
             if str(file).startswith("."):
                 continue
             self.assertTrue(self.kpro.name not in str(file))
-            if file.is_file():
+            if file.is_file() and file in whitelist:
                 with open(file, "r", encoding="latin-1") as f:
                     content = f.read()
                     self.assertTrue(self.kpro.name not in content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
