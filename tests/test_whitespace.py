46f55701fb1497bec7082fbb9e94276ca2c28b82
Hubert Stefaniak <hstefaniak@antmicro.com>
2024-11-15 15:11:34 +0100

    [#67440] Add whitespace test

---
+++ tests/test_whitespace.py
@@ -0,0 +1,29 @@
+import unittest
+import shutil
+import os
+
+from common.kicad_project import KicadProject
+from kmake_test_common import KmakeTestCase
+
+
+class WhitespaceTest(KmakeTestCase, unittest.TestCase):
+
+    def __init__(self, method_name: str = "runTest") -> None:
+
+        KmakeTestCase.__init__(self, "gerber")
+        unittest.TestCase.__init__(self, method_name)
+
+    def test_whitespace(self) -> None:
+
+        temp_dir = self.target_dir.with_name("test_project (copy)")
+        shutil.move(self.target_dir, temp_dir)
+        self.target_dir = temp_dir
+        os.chdir(self.target_dir)
+        self.kpro = KicadProject()
+        self.run_test_command([])
+        gerber_count = len(list(self.target_dir.joinpath("fab").glob("test_project-*.gbr")))
+        self.assertEqual(gerber_count, 39)
+
+
+if __name__ == "__main__":
+    unittest.main()
