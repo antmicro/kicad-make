65cd2964bb8eee1f2ccf73d5ef0034109ff3b2dc
Dawid Lentka <dlentka@antmicro.com>
2024-11-29 14:40:07 +0100

    [#69305] step: Ensure exported file has pcb color taken from stackup

--- src/commands/step.py
+++ src/commands/step.py
@@ -2,9 +2,11 @@
 
 import argparse
 import logging
+import re
 
 from common.kicad_project import KicadProject
 from common.kmake_helper import run_kicad_cli
+from kiutils.board import Board
 
 log = logging.getLogger(__name__)
 
@@ -22,11 +24,44 @@ def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
 
     log.info("Exporting 3D STEP as %s", output_file_path)
 
+    board = Board.from_file(kicad_project.pcb_file)
+    mask_color = [sl.color for sl in board.setup.stackup.layers if sl.name == "F.Mask"][0]
+
+    preset_colors = {
+        "Green": [20, 51, 36],
+        "Red": [181, 19, 21],
+        "Blue": [2, 59, 162],
+        "Purple": [32, 2, 53],
+        "Black": [11, 11, 11],
+        "White": [245, 245, 245],
+        "Yellow": [194, 195, 0],
+    }
+
+    if mask_color is None:
+        color = preset_colors["Green"]
+    elif mask_color.startswith("#"):
+        color = [int(mask_color[1:3], 16), int(mask_color[3:5], 16), int(mask_color[5:7], 16)]
+    else:
+        color = preset_colors.get(mask_color, preset_colors["Green"])
+    colorf = [c / 256 for c in color]
+
     export_step(
         kicad_project.pcb_file,
         output_file_path,
         verbose=args.debug,
     )
+
+    match_r = r"#(\d*) = COLOUR_RGB\('',(\d*\.\d*),(\d*\.\d*),(\d*\.\d*)\);"
+
+    def sub_color(m: re.Match) -> str:
+        if 0.3 < float(m.group(2)) < 0.33 and 0.47 < float(m.group(3)) < 0.5 and 0.4 < float(m.group(4)) < 0.42:
+            return f"#{m.group(1)} = COLOUR_RGB('',{colorf[0]},{colorf[1]},{colorf[2]});"
+        return m.group(0)
+
+    step_file = open(output_file_path).read()
+    step_file = re.sub(match_r, sub_color, step_file)
+    with open(output_file_path, mode="w") as sfile:
+        sfile.write(step_file)
 
 
 def export_step(
