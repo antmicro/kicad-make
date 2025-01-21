852d2c17a4fe74028c74a5943e074cfd837eb8a3
Szymon Magnuszewski <smagnuszewski@internships.antmicro.com>
2025-01-21 13:56:24 +0100

    [#71777] Aux origin edge shapes

--- src/commands/auxorigin.py
+++ src/commands/auxorigin.py
@@ -4,7 +4,7 @@ import logging
 import math
 from kiutils.board import Board
 from kiutils.schematic import Position
-from kiutils.items.gritems import GrCircle
+from kiutils.items.gritems import GrCircle, GrArc, GrPoly
 
 from common.kicad_project import KicadProject
 
@@ -51,6 +51,66 @@ def set_aux_axis_origin(board: Board, x: float, y: float) -> None:
     save_board(board)
 
 
+# Calculates center point and radius of the circle defined with an arc
+def calculate_circle(
+    start: tuple[float, float], mid: tuple[float, float], end: tuple[float, float]
+) -> tuple[float, float, float]:
+    # Determinant
+    det = (mid[0] - start[0]) * (end[1] - start[1]) - (end[0] - start[0]) * (mid[1] - start[1])
+    # Calculate square distances
+    a = pow(start[0], 2) + pow(start[1], 2)
+    b = pow(mid[0], 2) + pow(mid[1], 2)
+    c = pow(end[0], 2) + pow(end[1], 2)
+    # Calculate circle center - intersecting point of perpendicular bisectors of a triangle
+    # sides are the center point of the circle circumscribed on that triangle.
+    x = -((mid[1] - start[1]) * (c - a) - (end[1] - start[1]) * (b - a)) / (2 * det)
+    y = -((end[0] - start[0]) * (b - a) - (mid[0] - start[0]) * (c - a)) / (2 * det)
+    # Calculate radius - pythagorean theorem
+    r = math.hypot(start[0] - x, start[1] - y)
+    return x, y, r
+
+
+# Calculates arc extremum in x and y axes
+def find_arc_extrema(
+    circle_x: float,
+    circle_y: float,
+    r: float,
+    start: tuple[float, float],
+    mid: tuple[float, float],
+    end: tuple[float, float],
+) -> tuple[float, float, float, float]:
+    # Finds normalized angle in radians between selected point and circle mid point
+    def angle(x: float, y: float) -> float:
+        angle = math.atan2(y - circle_y, x - circle_x)
+        angle %= 2 * math.pi
+        return angle
+
+    # Verifies if angle is in range [start_angle, end_angle] in cartesian system
+    def is_angle_in_range(angle: float, start_angle: float, end_angle: float) -> bool:
+        if start_angle <= end_angle:
+            return start_angle <= angle <= end_angle
+        return angle >= start_angle or angle <= end_angle
+
+    # Calculates angles for arc defining points
+    start_angle = angle(start[0], start[1])
+    end_angle = angle(end[0], end[1])
+
+    # Add arc defining points as potential extremum
+    extrema = [start, mid, end]
+
+    # Add extremum occuring for arc on axes
+    for candidate_angle in [0, math.pi / 2, math.pi, 3 * math.pi / 2]:
+        x_extreme = circle_x + r * math.cos(candidate_angle)
+        y_extreme = circle_y + r * math.sin(candidate_angle)
+        if is_angle_in_range(candidate_angle, start_angle, end_angle):
+            extrema.append((x_extreme, y_extreme))
+
+    x = [point[0] for point in extrema]
+    y = [point[1] for point in extrema]
+
+    return max(x), min(x), max(y), min(y)
+
+
 def set_aux_origin_on_size(board: Board, side: str) -> None:
     log.info("Reading PCB dimmmensions")
     x = []
@@ -60,13 +120,31 @@ def set_aux_origin_on_size(board: Board, side: str) -> None:
             continue
         # Circle case
         if isinstance(item, GrCircle):
-            log.info("Detected circular PCB shape")
             # Coordinates of the square circumscribed by circle
-            r = math.hypot(abs(item.center.X - item.end.X), abs(item.center.Y - item.end.Y))
+            r = math.hypot(item.center.X - item.end.X, item.center.Y - item.end.Y)
             x.append(item.center.X + r)
             x.append(item.center.X - r)
             y.append(item.center.Y + r)
             y.append(item.center.Y - r)
+            continue
+        # Arc case
+        if isinstance(item, GrArc):
+
+            start = item.start.X, item.start.Y
+            mid = item.mid.X, item.mid.Y
+            end = item.end.X, item.end.Y
+            circ_x, circ_y, r = calculate_circle(start, mid, end)
+            max_x, min_x, max_y, min_y = find_arc_extrema(circ_x, circ_y, r, start, mid, end)
+            x.append(max_x)
+            x.append(min_x)
+            y.append(max_y)
+            y.append(min_y)
+            continue
+        # Poly case
+        if isinstance(item, GrPoly):
+            for point in item.coordinates:
+                x.append(point.X)
+                y.append(point.Y)
             continue
         # Rectangle, segment case
         if hasattr(item, "start"):
@@ -75,9 +153,6 @@ def set_aux_origin_on_size(board: Board, side: str) -> None:
         if hasattr(item, "end"):
             x.append(item.end.X)
             y.append(item.end.Y)
-        if hasattr(item, "mid"):
-            x.append(item.mid.X)
-            y.append(item.mid.Y)
 
     for footprint in board.footprints:
         if footprint.position is None:
@@ -91,12 +166,12 @@ def set_aux_origin_on_size(board: Board, side: str) -> None:
                 y.append(item.start.Y + footprint.position.Y)
                 y.append(item.end.Y + footprint.position.Y)
 
-    if "t" in side:
+    if "r" in side:
         aux_x = max(x)
     else:
         aux_x = min(x)
 
-    if "r" in side:
+    if "t" in side:
         aux_y = min(y)
     else:
         aux_y = max(y)
