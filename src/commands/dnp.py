1cde89d6ed55fd1ef21d4b1b400fe7dfb1322a46
Michal Kotkowski <mkotkowski@antmicro.com>
2026-03-05 22:25:30 +0100

    [#92089] Dnp update behavior

--- src/commands/dnp.py
+++ src/commands/dnp.py
@@ -24,7 +24,14 @@ def add_subparser(subparsers: argparse._SubParsersAction) -> None:
         "--list-broken",
         dest="list_broken",
         action="store_true",
-        help="List references of malformed DNP schematic components, do not modify.",
+        help="List malformed DNP schematic components. List discrepancies between PCB and SCH. Do not modify.",
+    )
+    parser.add_argument(
+        "-f",
+        "--fix-legacy",
+        dest="fix_legacy",
+        action="store_true",
+        help="Fix symbols and footprints with legacy DNP property fields.",
     )
     parser.add_argument(
         "-rp",
@@ -57,49 +64,66 @@ def add_subparser(subparsers: argparse._SubParsersAction) -> None:
     parser.set_defaults(func=run)
 
 
+def cleanup_sch(schpro: SchProject, malformed_components: List[SchematicSymbol]) -> None:
+    # Cleanup components
+    log.info(f"Fixing legacy schematic component{'s' if len(malformed_components) != 1 else ''}")
+    for component in malformed_components:
+        clean_up_component(component)
+    schpro.save()
+
+
+# Remove from PCB footprints additional properties doubling checkboxes functionality
+def cleanup_pcb(pcb: Board) -> None:
+    log.info("Fixing legacy PCB components")
+    for fp in pcb.footprints:
+        for prop in ["DNP", "dnp"]:
+            fp.properties = remove_property(fp, prop)
+
+
 def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
     # Read in all schematic files
     assert not (
         args.no_paste and args.set_paste
     ), "Only one of [`--remove-dnp-paste`, `--restore-dnp-paste`] can be specified"
+    assert not (args.list_broken and args.fix_legacy), "Only one of [`--list-broken`, `--fix-legacy`] can be specified"
 
+    broken_logs: list[str] = []
     schpro = kicad_project.sch_project()
 
     # Get all components that are marked DNP
     dnp_components = get_dnp_components(schpro)
-    log.debug(f"Found {len(dnp_components)} schematic components marked DNP")
+    log.debug(f"Found {len(dnp_components)} schematic component{'s' if len(dnp_components) != 1 else ''} marked DNP")
 
     # Count components that need cleanup
-    cleanup_count = sum(needs_cleanup(component) for component in dnp_components)
-    cleanup_list = [get_property(comp, "Reference") for comp in dnp_components if needs_cleanup(comp)]
-    if cleanup_count == 0 and args.list_broken:
-        log.info("Nothing to do")
-        return
-    if cleanup_count > 0 and args.list_broken:
-        log.warning(
-            f"There are {cleanup_count} schematic components that "
-            f"have their DNP properties malformed: [{' '.join(cleanup_list)}]."
+    cleanup_list = get_cleanup_components(schpro)
+    cleanup_count = len(cleanup_list)
+    if cleanup_count:
+        log_text = (
+            f"Malformed DNP propert{'ies' if cleanup_count != 1 else 'y'} found in "
+            f"{cleanup_count} schematic component{'s' if cleanup_count != 1 else ''}: "
+            f"{' '.join([get_property(comp,'Reference') for comp in cleanup_list])}."
         )
-        sys.exit(1)
-    if cleanup_count > 0:
-        log.info(f"There are {cleanup_count} schematic components that have their DNP properties malformed")
-        log.debug(f"[{' '.join(cleanup_list)}]")
-        # Cleanup components
-        log.info("Cleaning up schematic components")
-        for component in dnp_components:
-            clean_up_component(component)
+        if args.list_broken:
+            broken_logs.append(log_text)
+        else:
+            log.warning(log_text)
+        if args.fix_legacy:
+            cleanup_sch(schpro, cleanup_list)
+        else:
+            if args.list_broken:
+                broken_logs.append("Use `kmake dnp --fix-legacy` to fix schematics.")
+            else:
+                log.warning("Use `kmake dnp --fix-legacy` to fix schematics.")
 
-        schpro.save()
-
-    # Get references
-    log.debug("Searching for components on PCB")
-    references = []
-    for component in dnp_components:
-        references.append(get_property(component, "Reference"))
-        for instance in component.instances:
+    # Get symbol references from schematics
+    sym_dnp = []
+    log.debug("Collecting all symbol references in schematics.")
+    for sym in dnp_components:
+        sym_dnp.append(get_property(sym, "Reference"))
+        for instance in sym.instances:
             for path in instance.paths:
-                if path.reference not in references:
-                    references.append(path.reference)
+                if path.reference not in sym_dnp:
+                    sym_dnp.append(path.reference)
     # resolve sheet level dnp
     for schematic in schpro.schematics:
         for symbol in schematic.schematicSymbols:
@@ -107,65 +131,90 @@ def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
                 for path in instance.paths:
                     if path.reference.startswith("#"):
                         continue
-                    if path.reference not in references and any(
+                    if path.reference not in sym_dnp and any(
                         [uid in path.sheetInstancePath and prop.dnp for uid, prop in schpro.sheet_prop.items()]
                     ):
-                        references.append(path.reference)
-    log.debug(f"DNP references from schematic {sorted(references)}")
-
-    # Update PCB footprints
-    log.debug("Updating PCB")
+                        sym_dnp.append(path.reference)
+    log.debug(f"DNP references from schematic: {' '.join(sorted(sym_dnp))}")
 
     pcb = Board().from_file(kicad_project.pcb_file)
-    update_pcb(references, pcb, args.no_paste, args.set_paste, args.set_tht_paste, args.reset_tht_paste)
+    fp_dnp = find_dnp_footprints_on_pcb(pcb)
+    if args.list_broken:
+        for sym in sym_dnp:
+            if sym not in fp_dnp:
+                broken_logs.append(f"Component {sym} marked as DNP only in schematics")
+        for fp_ref in fp_dnp:
+            if fp_ref not in sym_dnp:
+                broken_logs.append(f"Component {fp_ref} marked as DNP only in PCB")
+        # Check additional properties doubling checkboxes functionality
+        for fp in pcb.footprints:
+            short_name = f"{get_property(fp, 'Reference')}: {fp.entryName}"
+            if fp.attributes.excludeFromBom:
+                broken_logs.append(f"Footprint {short_name} has an additional 'Exclude from BOM' attribute set")
+            if fp.attributes.excludeFromPosFiles:
+                broken_logs.append(
+                    f"Footprint {short_name} has an additional 'Exclude from position files' attribute set"
+                )
+            if get_property(fp, "DNP"):
+                broken_logs.append(f"Footprint {short_name} has an additional 'DNP' property set")
+        for text in broken_logs:
+            log.warning(text)
+        if broken_logs:
+            sys.exit(1)
+        sys.exit(0)
+    # Update PCB footprints
+    log.debug("Updating PCB")
+    if args.fix_legacy:
+        cleanup_pcb(pcb)
+    update_dnp_on_pcb(sym_dnp, pcb, args.no_paste, args.set_paste, args.set_tht_paste, args.reset_tht_paste)
     pcb.to_file()
-
     prettify(kicad_project, argparse.Namespace())
 
 
 def get_dnp_components(schpro: SchProject) -> List[SchematicSymbol]:
     components = []
     for schematic in schpro.schematics:
-        for symbol in schematic.schematicSymbols:
-            if is_dnp(symbol):
-                components.append(symbol)
+        components.extend([symbol for symbol in schematic.schematicSymbols if is_dnp(symbol)])
+    return components
+
+
+def get_cleanup_components(schpro: SchProject) -> List[SchematicSymbol]:
+    components = []
+    for schematic in schpro.schematics:
+        for comp in schematic.schematicSymbols:
+            if get_property(comp, "DNP") is not None:
+                components.append(comp)
     return components
 
 
 # Checks whether component is DNP based on DNP property and attribute
 def is_dnp(component: SchematicSymbol) -> bool:
-    dnp_property = get_property(component, "DNP")
-
-    if component.dnp:
-        return True
-
-    if dnp_property not in [None, "", "~"]:
-        return True
-
-    return False
-
-
-# Checks whether component needs cleanup
-def needs_cleanup(component: SchematicSymbol) -> bool:
-    if not component.dnp:
-        return True
-    prop = get_property(component, "DNP")
-    if prop is not None:
+    if component.dnp or get_property(component, "DNP") not in [None, "", "~"]:
         return True
     return False
 
 
-# Cleans up component - sets dnp and removes legacy DNP property
+# Cleans up component - sets DNP attribute and removes legacy DNP property
 def clean_up_component(component: SchematicSymbol) -> None:
-    component.dnp = True
-    # Replaces legacy DNP property with default kicad DNP checkbox
-    prop = get_property(component, "DNP")
-    if prop is not None:
-        component.properties = remove_property(component, "DNP")
+    dnp_field = get_property(component, "DNP")
+    if dnp_field is None:
+        return
+    component.properties = remove_property(component, "DNP")
+    if dnp_field not in [None, "", "~"]:
+        component.dnp = True
 
 
-# Updates footprints on pcb
-def update_pcb(
+# Find footprints set as DNP
+def find_dnp_footprints_on_pcb(board: Board) -> list[str]:
+    fp_dnp = []
+    for fp in board.footprints:
+        if fp.attributes.dnp and not fp.attributes.boardOnly:
+            fp_dnp.append(get_property(fp, "Reference"))
+    return fp_dnp
+
+
+# Updates DNP property on PCB footprints
+def update_dnp_on_pcb(
     references: List[str],
     board: Board,
     remove_paste: bool,
@@ -182,23 +231,15 @@ def update_pcb(
             add_tht_paste(footprint)
         if tht_paste_restore:
             remove_tht_paste(footprint)
-        if get_property(footprint, "Reference") in references:
-            set_fp_dnp(footprint, remove_paste, restore_paste)
-        elif not footprint.attributes.boardOnly and get_property(footprint, "MPN"):
-            footprint.attributes.excludeFromPosFiles = False
-            footprint.attributes.dnp = False
-            restore_fp_paste(footprint)
-        # Remove additional properties doubling checkboxes functionality
-        for prop in ["DNP", "dnp", "exclude_from_bom"]:
-            footprint.properties = remove_property(footprint, prop)
+        set_fp_dnp_state(footprint, get_property(footprint, "Reference") in references, remove_paste, restore_paste)
 
 
-# Updates footprint to have dnp field and appropriate attributes
-def set_fp_dnp(footprint: Footprint, remove_paste: bool, restore_paste: bool) -> None:
-    log.debug(f"Setting {get_property(footprint, 'Reference')} to DNP")
-    footprint.attributes.excludeFromPosFiles = True
-    footprint.attributes.dnp = True
-    if remove_paste:
+# Updates footprint to have dnp field
+def set_fp_dnp_state(footprint: Footprint, dnp_state: bool, remove_paste: bool, restore_paste: bool) -> None:
+    if not footprint.attributes.boardOnly and footprint.attributes.dnp != dnp_state:
+        footprint.attributes.dnp = dnp_state
+        log.debug(f"Setting {get_property(footprint, 'Reference')} DNP to: {dnp_state}")
+    if remove_paste and dnp_state:
         remove_fp_paste(footprint)
     if restore_paste:
         restore_fp_paste(footprint)
--- tests/test_dnp.py
+++ tests/test_dnp.py
@@ -21,10 +21,10 @@ class DnpTest(KmakeTestCase, unittest.TestCase):
         self.reset_repo()
 
     def check_symbol(self, components: List[str], dnp: bool, dnp_field: bool = False) -> None:
-        """Check if symbol have DNP fields
+        """Check if symbols have DNP attribute
 
         Parameters:
-            components: List of designator to check
+            components: List of designators to check
             dnp: Define if component is DNP
             dnp_field: Allow component to have a DNP field (when dnp is set to False)
         """
@@ -50,7 +50,7 @@ class DnpTest(KmakeTestCase, unittest.TestCase):
         self.assertEqual(components_checked, len(components), "Not all components checked, internal test error")
 
     def check_footprint(self, components: List[str], dnp: bool) -> None:
-        """Check if footprints have `Exclude from position files` and `Exclude from bill of material` fields valid
+        """Check if footprints have `Do not populate` attribute valid
 
         Parameters:
             component: List of designators to check
@@ -62,14 +62,13 @@ class DnpTest(KmakeTestCase, unittest.TestCase):
         footprints_checked = 0
         for footprint_id in range(0, footprints_count):
             footprint = footprints[footprint_id]
-            attributes = footprint.attributes
             designator = get_property(footprint, "Reference")
             if designator in components:
                 if dnp:
-                    self.assertEqual(attributes.excludeFromPosFiles, True, f"{designator} Not excluded from POS files")
+                    self.assertEqual(footprint.attributes.dnp, True, f"{designator} not set to 'Do not populate'")
                     footprints_checked += 1
                 else:
-                    self.assertEqual(attributes.excludeFromPosFiles, False, f"{designator} Excluded from POS files")
+                    self.assertEqual(footprint.attributes.dnp, False, f"{designator} set to 'Do not populate'")
                     footprints_checked += 1
         self.assertEqual(footprints_checked, len(components), "Not all components checked internal test error")
 
@@ -98,34 +97,34 @@ class DnpTest(KmakeTestCase, unittest.TestCase):
             if designator in components:
                 paste_counter = self.check_paste_layer(footprint)
                 if dnp:
-                    self.assertEqual(paste_counter, 0, "Paste wasnt removed from all pads")
+                    self.assertEqual(paste_counter, 0, "Paste wasn't removed from all pads")
                 else:
                     self.assertEqual(paste_counter, len(footprint.pads))
                 footprints_checked += 1
         self.assertEqual(len(components), footprints_checked, "Not all components checked internal test error")
 
     def test_list_malformed(self) -> None:
-        """Test output for -l command (list malformed)"""
+        """Test output for only listing malformed DNP properties"""
         with self.assertLogs(level=logging.WARNING) as log:
             with self.assertRaises(SystemExit) as se:
                 self.run_test_command(["-l"])
         self.assertIn(
-            "There are 2 schematic components that have their DNP properties malformed:",
-            log.output[0][18:96],
+            "Malformed DNP properties found in 2 schematic components:",
+            log.output[0][21:78],
         )
         self.assertEqual(se.exception.code, 1)
 
     def test_clean_symbol(self) -> None:
-        "Test if dnp symbols have `Exlude from bill of materials` and `Do not populate` fields set correctly"
+        "Test if dnp symbols have `Do not populate` attribute set correctly"
         self.check_symbol(["R1", "R2"], False, True)
         self.check_symbol(["R3"], True, False)
         self.check_symbol(["C26", "C27"], False, False)
-        self.run_test_command([])
+        self.run_test_command(["--fix-legacy"])
         self.check_symbol(["R1", "R2", "R3"], True, False)
         self.check_symbol(["C26", "C27"], False, False)
 
     def test_clean_footprint(self) -> None:
-        "Test if DNP footprints have `Exclude from pos files` and `Exclude from bill of material` fields set correctly"
+        "Test if DNP footprints have `Do not populate` attribute set correctly"
         self.check_footprint(["R1"], False)
         self.check_footprint(["R6"], False)
         self.run_test_command([])
@@ -133,7 +132,7 @@ class DnpTest(KmakeTestCase, unittest.TestCase):
         self.check_footprint(["R6"], False)
 
     def test_remove_restore_paste(self) -> None:
-        "Test if solder pasted was removed and restored from DNP components"
+        "Test if solder paste was removed and restored from DNP components"
         self.check_paste(["R1"], False)
         self.check_paste(["C26"], False)
         self.run_test_command(["--remove-dnp-paste"])
@@ -171,8 +170,8 @@ class DnpTest(KmakeTestCase, unittest.TestCase):
 
         pcb = Board().from_file(self.kpro.pcb_file)
         for fp in pcb.footprints:
-            ref = get_property(fp, "Reference")
-            if ref == "R1":
-                fp.attributes.excludeFromPosFiles = False
+            if get_property(fp, "Reference") == "R1":
+                fp.attributes.dnp = False
+                fp.attributes.excludeFromBom = True
         pcb.to_file()
         prettify(self.kpro, argparse.Namespace())
