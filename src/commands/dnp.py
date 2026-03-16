1aac19636ad71bc07af054955c16e65032268aee
Marcel Nowicki <mnowicki@antmicro.com>
2026-03-16 15:57:42 +0100

    Port dnp.py

--- src/commands/dnp.py
+++ src/commands/dnp.py
@@ -2,13 +2,14 @@ import argparse
 import logging
 from typing import List
 import sys
+from pathlib import Path
 
-from kiutils.board import Board
+from askiff.kistruct.board import Board
+from askiff.kistruct.footprint import Footprint
 from kiutils.items.schitems import SchematicSymbol
 
 from common.kicad_project import KicadProject, SchProject
 from common.kmake_helper import get_property, remove_property
-from .prettify import run as prettify
 
 log = logging.getLogger(__name__)
 
@@ -48,12 +49,13 @@ def cleanup_pcb(pcb: Board) -> None:
     log.info("Fixing legacy PCB components")
     for fp in pcb.footprints:
         for prop in ["DNP", "dnp"]:
-            fp.properties = remove_property(fp, prop)
+            fp.properties.pop(prop)
 
 
 def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
     # Read in all schematic files
-    assert not (args.list_broken and args.fix_legacy), "Only one of [`--list-broken`, `--fix-legacy`] can be specified"
+    if args.list_broken and args.fix_legacy:
+        raise RuntimeError("Only one of [`--list-broken`, `--fix-legacy`] can be specified")
 
     broken_logs: list[str] = []
     schpro = kicad_project.sch_project()
@@ -65,16 +67,19 @@ def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
     # Count components that need cleanup
     cleanup_list = get_cleanup_components(schpro)
     cleanup_count = len(cleanup_list)
+
     if cleanup_count:
         log_text = (
             f"Malformed DNP propert{'ies' if cleanup_count != 1 else 'y'} found in "
             f"{cleanup_count} schematic component{'s' if cleanup_count != 1 else ''}: "
-            f"{' '.join([get_property(comp,'Reference') for comp in cleanup_list])}."
+            f"{' '.join([get_property(comp, 'Reference') for comp in cleanup_list])}."
         )
+
         if args.list_broken:
             broken_logs.append(log_text)
         else:
             log.warning(log_text)
+
         if args.fix_legacy:
             cleanup_sch(schpro, cleanup_list)
         else:
@@ -92,6 +97,7 @@ def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
             for path in instance.paths:
                 if path.reference not in sym_dnp:
                     sym_dnp.append(path.reference)
+
     # resolve sheet level dnp
     for schematic in schpro.schematics:
         for symbol in schematic.schematicSymbols:
@@ -105,8 +111,9 @@ def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
                         sym_dnp.append(path.reference)
     log.debug(f"DNP references from schematic: {' '.join(sorted(sym_dnp))}")
 
-    pcb = Board().from_file(kicad_project.pcb_file)
+    pcb = Board().from_file(Path(kicad_project.pcb_file))
     fp_dnp = find_dnp_footprints_on_pcb(pcb)
+
     if args.list_broken:
         for sym in sym_dnp:
             if sym not in fp_dnp:
@@ -114,29 +121,32 @@ def run(kicad_project: KicadProject, args: argparse.Namespace) -> None:
         for fp_ref in fp_dnp:
             if fp_ref not in sym_dnp:
                 broken_logs.append(f"Component {fp_ref} marked as DNP only in PCB")
+
         # Check additional properties doubling checkboxes functionality
         for fp in pcb.footprints:
-            short_name = f"{get_property(fp, 'Reference')}: {fp.entryName}"
-            if fp.attributes.excludeFromBom:
+            value_prop = fp.properties.get("Value")
+            short_name = f"{fp.properties.ref.value}: {value_prop.value if value_prop else ''}"
+
+            if fp.attributes.exclude_from_bom:
                 broken_logs.append(f"Footprint {short_name} has an additional 'Exclude from BOM' attribute set")
-            if fp.attributes.excludeFromPosFiles:
+            if fp.attributes.exclude_from_pos_files:
                 broken_logs.append(
                     f"Footprint {short_name} has an additional 'Exclude from position files' attribute set"
                 )
-            if get_property(fp, "DNP"):
+            if fp.properties.get("DNP"):
                 broken_logs.append(f"Footprint {short_name} has an additional 'DNP' property set")
         for text in broken_logs:
             log.warning(text)
         if broken_logs:
             sys.exit(1)
         sys.exit(0)
+
     # Update PCB footprints
     log.debug("Updating PCB")
     if args.fix_legacy:
         cleanup_pcb(pcb)
     update_dnp_on_pcb(sym_dnp, pcb)
     pcb.to_file()
-    prettify(kicad_project, argparse.Namespace())
 
 
 def get_dnp_components(schpro: SchProject) -> List[SchematicSymbol]:
@@ -176,15 +186,22 @@ def clean_up_component(component: SchematicSymbol) -> None:
 def find_dnp_footprints_on_pcb(board: Board) -> list[str]:
     fp_dnp = []
     for fp in board.footprints:
-        if fp.attributes.dnp and not fp.attributes.boardOnly:
-            fp_dnp.append(get_property(fp, "Reference"))
+        if fp.attributes.dnp and not fp.attributes.board_only:
+            fp_dnp.append(fp.properties.ref.value)
     return fp_dnp
 
 
 # Updates DNP property on PCB footprints
-def update_dnp_on_pcb(references: List[str], board: Board) -> None:
+def update_dnp_on_pcb(
+    references: List[str],
+    board: Board,
+) -> None:
     for footprint in board.footprints:
-        dnp_state = get_property(footprint, "Reference") in references
-        if not footprint.attributes.boardOnly and footprint.attributes.dnp != dnp_state:
-            footprint.attributes.dnp = dnp_state
-            log.debug(f"Setting {get_property(footprint, 'Reference')} DNP to: {dnp_state}")
+        set_fp_dnp_state(footprint, footprint.properties.ref.value in references)
+
+
+# Updates footprint to have dnp field
+def set_fp_dnp_state(footprint: Footprint, dnp_state: bool) -> None:
+    if not footprint.attributes.board_only and footprint.attributes.dnp != dnp_state:
+        footprint.attributes.dnp = dnp_state
+        log.debug(f"Setting {footprint.properties.ref.value} DNP to: {dnp_state}")
