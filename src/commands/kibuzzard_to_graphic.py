e6f5be71273436d240a2ebbcedc154f4503d3c69
Dawid Lentka <dlentka@antmicro.com>
2026-04-20 15:45:23 +0200

    [#93586] kibuzzard_to_graphics: migrate to askiff

--- src/commands/kibuzzard_to_graphic.py
+++ src/commands/kibuzzard_to_graphic.py
@@ -1,13 +1,11 @@
 """kibuzzard-to-graphic"""
 
-import logging
 import argparse
-from askiff.board import Board
-from askiff.gritems import GrPoly, GrPolyFp
+import logging
 
-from math import sin, cos, radians
-from pathlib import Path
-from common.kicad_project import KicadProject
+from askiff import Project
+from askiff.gritems import GrPolyFp
+
 
 log = logging.getLogger(__name__)
 
@@ -18,54 +16,29 @@ def add_subparser(subparsers: argparse._SubParsersAction) -> None:
     parser.set_defaults(func=run)
 
 
-def main(pro: KicadProject, args: argparse.Namespace) -> None:
+def kibuzzard_to_graphic(pro: Project, _args: argparse.Namespace) -> None:
     """Main module function"""
 
-    pcb_path = pro.pcb_file
-    board = Board().from_file(Path(pcb_path))
+    for pcb in pro.pcb:
+        footprints_to_remove = []
 
-    footprints_to_remove = []
-
-    for footprint in board.footprints:
-        if not footprint.lib_id.name.startswith("kibuzzard") or not footprint.graphic_items:
-            continue
-        log.debug(f"KiBuzzard footprint found ({footprint.entry_name})")
-
-        footprints_to_remove.append(footprint)
-        for fp_item in footprint.graphic_items:
-            if not isinstance(fp_item, GrPolyFp):
+        for footprint in pcb.footprints:
+            if not footprint.lib_id.name.startswith("kibuzzard") or not footprint.graphic_items:
                 continue
+            log.debug(f"KiBuzzard footprint found ({footprint.entry_name})")
 
-            # Append position to offset coordinates
-            gr_poly = GrPoly()
-            gr_poly.coordinates = fp_item.coordinates.copy()
+            footprints_to_remove.append(footprint)
+            pcb.graphic_items.extend(
+                item.to_shape_pcb(footprint.position) for item in footprint.graphic_items if isinstance(item, GrPolyFp)
+            )
 
-            for pos in gr_poly.coordinates:
-                # Add footprint offset and rotation to gr_poly coordinates
-                rotation = footprint.position.angle
-                if not rotation:
-                    rotation = 0
+        for footprint in footprints_to_remove:
+            pcb.footprints.remove(footprint)
+            log.debug(f"Deleted KiBuzzard footprint ({footprint.entry_name})")
 
-                x_angle_offset = pos.x * cos(radians(rotation)) + pos.y * sin(radians(rotation))
-                y_angle_offset = pos.y * cos(radians(rotation)) - pos.x * sin(radians(rotation))
-                pos.x = x_angle_offset
-                pos.y = y_angle_offset
-                pos.x += footprint.position.x
-                pos.y += footprint.position.y
-
-            gr_poly.layer = fp_item.layer
-            gr_poly.width = fp_item.stroke.width
-            gr_poly.fill = fp_item.stroke.type
-            board.graphic_items.append(gr_poly)
-            log.debug("Created graphical polygon from KiBuzzard footprint")
-
-    for footprint in footprints_to_remove:
-        board.footprints.remove(footprint)
-        log.debug(f"Deleted KiBuzzard footprint ({footprint.entry_name})")
-
-    board.to_file(Path(pcb_path))
+        pcb.to_file()
 
 
-def run(project: KicadProject, args: argparse.Namespace) -> None:
+def run(project: Project, args: argparse.Namespace) -> None:
     """Entry function for module"""
-    main(project, args)
+    kibuzzard_to_graphic(project, args)
