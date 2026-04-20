99e183f0a5ede1bfe49055114c06403ac5ac083a
Dawid Lentka <dlentka@antmicro.com>
2026-04-20 17:13:51 +0200

    [#93040] globlib: migrate to askiff

--- src/commands/globlib.py
+++ src/commands/globlib.py
@@ -1,26 +1,20 @@
 import argparse
 import logging
 import os
-from pathlib import Path
-from typing import Dict, Iterable, List, Optional, Tuple, Union
 import re
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Union
 
-from askiff import Board, Project, Schematic
-from askiff.footprint import Footprint, FootprintFile, LibId
-from askiff.symbol import Symbol, SymbolLib
+from askiff import FootprintFile, Project, Schematic, SymbolFile
+from askiff.footprint import Footprint, LibId
+from askiff.symbol import SymbolDefinition, SymbolSchematic
 
 from common.kicad_project import KicadProject
-from common.kmake_helper import get_property, set_property
 
 log = logging.getLogger(__name__)
 
-UniSymbol = Union[SchematicSymbol, Symbol]
-
-
-class SymbolEntry:
-    def __init__(self, symbol: Symbol, library_name: str) -> None:
-        self.symbol: Symbol = symbol
-        self.library_name: str = library_name
+Symbol = Union[SymbolDefinition, SymbolSchematic]
 
 
 def add_subparser(subparsers: argparse._SubParsersAction) -> None:
@@ -64,7 +58,7 @@ def run(pro: KicadProject, args: argparse.Namespace) -> None:
 
 def get_lib_mapping(
     pro: KicadProject, include_kicad_lib: bool, lib_table_file: str, system_table_file: str, lib_dir: str
-) -> Dict[str, str]:
+) -> dict[str, str]:
     """Returns dict mapping symbol library names to paths based on user's kicad config."""
     libtable = pro.read_lib_table_file(lib_table_file, system_table_file)
 
@@ -78,29 +72,21 @@ def get_lib_mapping(
     return {lib.name: os.path.expandvars(lib.uri) for lib in libtable.libs}
 
 
-def get_symbol_name(symbol: UniSymbol) -> str:
-    symbol_name = symbol.libId.split(":", 1)
-    if len(symbol_name) == 1:
-        return symbol.libId
-    return symbol_name[1]
-
-
-def get_global_symbol_list(lib_mapping: Dict[str, str]) -> Dict[str, Tuple[str, UniSymbol]]:
-    sym_list: dict[str, Tuple[str, UniSymbol]] = {}
+def get_global_symbol_list(lib_mapping: dict[str, str]) -> dict[str, tuple[str, SymbolDefinition]]:
+    sym_list = {}
     for lib_name, path in lib_mapping.items():
         if not os.path.exists(path):
             log.warning(f"Library {lib_name} points to file that does not exist. Library will be omitted.")
             continue
-        sym_library = SymbolLib.from_file(path)
+        sym_library = SymbolFile.from_file(path)
         log.debug(f"Parsing global library: {lib_name}")
         for symbol in sym_library.symbols:
-            sym_name = get_symbol_name(symbol)
-            sym_list[sym_name] = (lib_name, symbol)
+            sym_list[symbol.lib_id.name] = (lib_name, symbol)
     return sym_list
 
 
-def get_global_footprint_list(lib_mapping: Dict[str, str]) -> Dict[str, Tuple[str, Footprint]]:
-    fp_list: dict[str, Tuple[str, Footprint]] = {}
+def get_global_footprint_list(lib_mapping: dict[str, str]) -> dict[str, tuple[str, Footprint]]:
+    fp_list: dict[str, tuple[str, Footprint]] = {}
     for lib_name, path in lib_mapping.items():
         if not os.path.exists(path):
             log.warning(f"Library {lib_name} points to file that does not exist. Library will be omitted.")
@@ -121,31 +107,30 @@ def normalize_mpn(mpn: str) -> str:
 
 
 def search_by_mpn(
-    local_symbol: UniSymbol,
-    global_symbols: Dict[str, Tuple[str, UniSymbol]],
-) -> Optional[Tuple[str, UniSymbol]]:
-    local_mpn = get_property(local_symbol, "MPN")
-    local_name = get_symbol_name(local_symbol)
+    local_symbol: Symbol,
+    global_symbols: dict[str, tuple[str, SymbolDefinition]],
+) -> tuple[str, Symbol] | None:
+    local_mpn = local_symbol.properties.get_value("MPN")
 
     if not local_mpn or local_mpn == "":
-        log.warning("Symbol: %s has no mpn to match.", local_name)
+        log.warning("Symbol: %s has no mpn to match.", local_symbol.lib_id.name)
         return None
 
-    matching_symbols: List[Tuple[str, UniSymbol]] = []
+    matching_symbols: list[tuple[str, Symbol]] = []
 
     for _, (global_lib_name, global_symbol) in global_symbols.items():
-        global_mpn = get_property(global_symbol, "MPN")
+        global_mpn = global_symbol.properties.get_value("MPN") or ""
         if not global_mpn and normalize_mpn(global_mpn) == normalize_mpn(local_mpn):
             matching_symbols.append((global_lib_name, global_symbol))
 
     if not matching_symbols:
-        log.warning("Symbol: %s not found in global libraries.", local_name)
+        log.warning("Symbol: %s not found in global libraries.", local_symbol.lib_id.name)
         return None
 
     if len(matching_symbols) >= 2:
         log.warning(
             "Multiple replacements found for symbol named: %s with MPN: %s",
-            local_name,
+            local_symbol.lib_id.name,
             local_mpn,
         )
         return None
@@ -154,19 +139,20 @@ def search_by_mpn(
 
 
 def update_props(
-    local_symbol: UniSymbol,
-    global_symbol: UniSymbol,
+    local_symbol: Symbol,
+    global_symbol: Symbol,
     global_lib: str,
     all_props: bool,
 ) -> None:
-    local_symbol.libId = f"{global_lib}:{global_symbol.libId}"
+    local_symbol.lib_id.library = global_lib
+    local_symbol.lib_id.name = global_symbol.lib_id.name
     for global_property in global_symbol.properties:
         for local_property in local_symbol.properties:
-            if local_property.key == "Reference":
+            if local_property.name == "Reference":
                 continue
-            if (local_property.key != "Footprint") and not all_props:
+            if (local_property.name != "Footprint") and not all_props:
                 continue
-            if local_property.key == global_property.key:
+            if local_property.name == global_property.name:
                 local_property.value = global_property.value
 
 
@@ -177,38 +163,36 @@ def get_sch_paths_based_on_args(args: argparse.Namespace, pro: Project) -> list[
 
 
 def find_global_symbol(
-    local_symbol: UniSymbol, global_symbols: dict[str, Tuple[str, UniSymbol]]
-) -> Optional[Tuple[str, UniSymbol]]:
-    local_symbol_name = get_symbol_name(local_symbol)
-    log.debug("Processing symbol: %s", local_symbol_name)
+    local_symbol: Symbol,
+    global_symbols: dict[str, tuple[str, SymbolDefinition]],
+) -> tuple[str, Symbol] | None:
+    log.debug("Processing symbol: %s", local_symbol.lib_id.name)
 
-    if local_symbol_name in global_symbols:
-        global_symbol_name = local_symbol_name
-        global_symbol_lib = global_symbols[global_symbol_name][0]
-        global_symbol = global_symbols[global_symbol_name][1]
-        log.debug("Symbol with name: %s found in global library: %s", global_symbol_name, global_symbol_lib)
+    if local_symbol.lib_id.name in global_symbols:
+        global_symbol_lib = global_symbols[local_symbol.lib_id.name][0]
+        global_symbol = global_symbols[local_symbol.lib_id.name][1]
+        log.debug("Symbol with name: %s found in global library: %s", local_symbol.lib_id.name, global_symbol_lib)
     else:
         result = search_by_mpn(local_symbol, global_symbols)
         if result is not None:
             global_symbol_lib, global_symbol = result
-            log.debug("Symbol with libId: %s found in global library by MPN", local_symbol.libId)
+            log.debug("Symbol with lib_id: %s found in global library by MPN", local_symbol.lib_id)
         else:
-            log.warning("Symbol with libId: %s wasn't found in global library", local_symbol.libId)
+            log.warning("Symbol with lib_id: %s wasn't found in global library", local_symbol.lib_id)
             return None
 
     return global_symbol_lib, global_symbol
 
 
-def should_symbol_be_globlibed(symbol: UniSymbol, global_libraries: Iterable[str], update_all: bool) -> bool:
-    split = symbol.libId.split(":")
-    if len(split) <= 1:  # If there is no : in libId, symbol is locally edited and shouldn't be globlibed
+def should_symbol_be_globlibed(symbol: Symbol, global_libraries: Iterable[str], update_all: bool) -> bool:
+    if symbol.lib_id.library is None:  # If there is no : in lib_id, symbol is locally edited and shouldn't be globlibed
         return False
-    if split[0] in global_libraries and not update_all:
+    if symbol.lib_id.library in global_libraries and not update_all:
         return False
     return True
 
 
-def globlib_project_symbols(pro: KicadProject, args: argparse.Namespace) -> list[UniSymbol]:
+def globlib_project_symbols(pro: KicadProject, args: argparse.Namespace) -> list[Symbol]:
     library_mapping = get_lib_mapping(
         pro,
         args.include_kicad_lib,
@@ -221,7 +205,7 @@ def globlib_project_symbols(pro: KicadProject, args: argparse.Namespace) -> list
     log.info("Generating global symbol list.")
     global_symbols = get_global_symbol_list(library_mapping)
 
-    failures: list[UniSymbol] = []
+    failures: list[Symbol] = []
 
     for schematic in get_sch_paths_based_on_args(args, pro):
         log.info("Processing schematic: %s", schematic.path)
@@ -235,7 +219,7 @@ def globlib_project_symbols(pro: KicadProject, args: argparse.Namespace) -> list
                 continue
             update_props(local_symbol, result[1], result[0], args.update_properties)
 
-        for local_symbol in schematic.libSymbols:
+        for local_symbol in schematic.lib_symbols:
             if not should_symbol_be_globlibed(local_symbol, library_mapping.keys(), args.update_all):
                 continue
             result = find_global_symbol(local_symbol, global_symbols)
@@ -248,19 +232,19 @@ def globlib_project_symbols(pro: KicadProject, args: argparse.Namespace) -> list
     return failures
 
 
-def update_fp_props(source: SchematicSymbol, ref: str, fp: Footprint, update_all: bool) -> Tuple[bool, bool]:
+def update_fp_props(source: SymbolSchematic, ref: str, fp: Footprint, update_all: bool) -> tuple[bool, bool]:
     changed = False
     if fp.properties.get("Reference") != ref:
         return (False, False)
     ofp = fp.lib_id
-    nfp = get_property(source, "Footprint")
-    fp.lib_id.name = nfp
+    nfp = source.properties.get_value("Footprint")
+    fp.lib_id.name = nfp or ""
     if nfp != ofp:
         log.debug("Changed %s footprint: %s -> %s", ref, ofp, nfp)
         changed = True
     if update_all:
         for sch_prop in source.properties:
-            set_property(fp, sch_prop.key, sch_prop.value)
+            fp.properties.set(sch_prop.name, sch_prop.value)
 
     return (True, changed)
 
@@ -277,31 +261,31 @@ def globlib_footprints(pro: KicadProject, args: argparse.Namespace) -> None:
     )
     fp_list = get_global_footprint_list(lib_mapping)
     log.info("Loading PCB ...")
-    pcb = Board().from_file(Path(pro.pcb_file))
-    log.info("Updating footprint links")
+    for pcb in pro.pcb:
+        log.info("Updating footprint links")
 
-    for schematic in get_sch_paths_based_on_args(args, pro):
-        log.info("Processing schematic: %s", schematic.path)
-        for schematic_symbol in schematic.symbols:
-            ref = get_property(schematic_symbol, "Reference")
-            log.debug("Processing:  %s", ref)
-            for fp in pcb.footprints:
-                (found, changed) = update_fp_props(schematic_symbol, ref, fp, args.update_properties)
-                if changed:
+        for schematic in get_sch_paths_based_on_args(args, pro):
+            log.info("Processing schematic: %s", schematic.path)
+            for schematic_symbol in schematic.symbols:
+                ref = schematic_symbol.properties.ref.value
+                log.debug("Processing:  %s", ref)
+                for fp in pcb.footprints:
+                    (found, changed) = update_fp_props(schematic_symbol, ref, fp, args.update_properties)
+                    if changed:
+                        changes += 1
+                    if found:
+                        break
+
+        # below iteration has 2 purposes: 1. to globlib footprints that are not in schematic; 2. to update 3D model link
+        for fp in pcb.footprints:
+            for globname, (globlib, globfp) in fp_list.items():
+                if globname != fp.lib_id.name:
+                    continue
+                if fp.lib_id.library not in lib_mapping:
                     changes += 1
-                if found:
-                    break
-
-    # below iteration has 2 purposes: 1. to globlib footprints that are not in schematic; 2. to update 3D model links
-    for fp in pcb.footprints:
-        for globname, (globlib, globfp) in fp_list.items():
-            if globname != fp.lib_id.name:
-                continue
-            if fp.lib_id.library not in lib_mapping:
-                changes += 1
-                fp.lib_id = LibId(globlib, globname)
-            fp.models = globfp.models
-    pcb.to_file()
+                    fp.lib_id = LibId(globlib, globname)
+                fp.models = globfp.models
+        pcb.to_file()
     log.info("Footprint links updated: %d", changes)
 
 
